77d2560e2087bfb43368780d5df5e01dd87f5430
GNi33 <theill182@gmail.com>
2019-01-20 12:20:37 +0100

    <day03> add Fabric class

---
+++ src/main/kotlin/app/factory/model/Fabric.kt
@@ -0,0 +1,33 @@
+package app.factory.model
+
+class Fabric<T> (val xSize: Int, val ySize: Int, val array: Array<Array<T>>) {
+
+    companion object {
+        inline operator fun <reified T> invoke() = Fabric(1000, 1000, Array(1000) { arrayOfNulls<T>(1000) })
+
+        inline operator fun <reified T> invoke(xWidth: Int, yWidth: Int, operator: (Int, Int) -> (T)): Fabric<T> {
+            val array = Array(xWidth) {
+                val x = it
+                Array(yWidth) { it1 -> operator(x, it1)}
+            }
+            return Fabric(xWidth, yWidth, array)
+        }
+    }
+
+    operator fun get(x: Int, y: Int): T {
+        return array[x][y]
+    }
+
+    operator fun set(x: Int, y: Int, t: T) {
+        array[x][y] = t
+    }
+
+    inline fun forEach(operation: (T) -> Unit) {
+        array.forEach { it -> it.forEach { operation.invoke(it) } }
+    }
+
+    inline fun forEachIndexed(operation: (x: Int, y: Int, T) -> Unit) {
+        array.forEachIndexed { x, p -> p.forEachIndexed { y, t -> operation.invoke(x, y, t) } }
+    }
+
+}
