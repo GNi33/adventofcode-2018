1cef0b5c591beb0a869692f304c1087a8fc71c8d
GNi33 <theill182@gmail.com>
2019-02-02 01:46:52 +0100

    5c4a3fd4e1c7eb214258aa8a Day 04 - Parse Input
    move date parsing into method

--- src/main/kotlin/app/factory/services/GuardSpy.kt
+++ src/main/kotlin/app/factory/services/GuardSpy.kt
@@ -1,24 +1,17 @@
 package app.factory.services
 
 import java.text.SimpleDateFormat
+import java.util.*
 
 class GuardSpy : IGuardSpy{
 
     override fun sortInput(lines: List<String>): List<String> {
 
-        val match = Regex("(15\\d\\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]) ([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])")
-
         return lines.sortedWith(object : Comparator<String> {
             override fun compare(o1: String?, o2: String?): Int {
                 if (o1 != null && o2 != null) {
-                    val regexResult1 = match.find(o1)
-                    val dateString1 = regexResult1!!.value
-
-                    val regexResult2 = match.find(o2)
-                    val dateString2 = regexResult2!!.value
-
-                    val date1 = SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateString1)
-                    val date2 = SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateString2)
+                    val date1 = parseDate(o1)
+                    val date2 = parseDate(o2)
 
                     if (date1 > date2) {
                         return 1
@@ -35,5 +28,14 @@ class GuardSpy : IGuardSpy{
             }
 
         })
+    }
+
+    private fun parseDate(line: String): Date {
+        val match = Regex("(15\\d\\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]) ([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])")
+
+        val regexResult1 = match.find(line)
+        val dateString1 = regexResult1!!.value
+
+        return SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateString1)
     }
 }
