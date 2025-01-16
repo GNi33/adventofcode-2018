d3eb2fa67318ad6af7df1aeb8b51cffd025496c5
GNi33 <theill182@gmail.com>
2025-01-16 01:59:04 +0100

    Day 17 - init

---
+++ src/main/kotlin/app/days/Day17.kt
@@ -0,0 +1,18 @@
+package app.days
+
+import app.util.InputReader
+import mu.KotlinLogging
+
+class Day17: IDay {
+
+    private val logger = KotlinLogging.logger {}
+    private val inputReader = InputReader()
+
+
+    override fun run() {
+        logger.info { "Day 17 - Reservoir Research" }
+        logger.info { "Part 01" }
+
+        logger.info { "Part 02" }
+    }
+}
