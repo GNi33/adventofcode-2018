9b9c8a7906170cd265148004767d5430193bd0d5
GNi33 <theill182@gmail.com>
2025-02-15 21:49:49 +0100

    Day 19 - Init

---
+++ src/main/kotlin/app/days/Day19.kt
@@ -0,0 +1,17 @@
+package app.days
+
+import app.util.InputReader
+import mu.KotlinLogging
+
+class Day19: IDay {
+    private val logger = KotlinLogging.logger {}
+    private val inputReader = InputReader()
+
+    override fun run() {
+        logger.info { "Day 19 - Go With The Flow" }
+        logger.info { "Part 01" }
+
+        logger.info { "Part 02" }
+
+    }
+}
