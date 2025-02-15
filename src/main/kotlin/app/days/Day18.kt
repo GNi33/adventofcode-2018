60a812299792b6b1fafab32a1b14b4454596c1f7
GNi33 <theill182@gmail.com>
2025-02-15 21:09:04 +0100

    Day 18 - Solve Part 02

--- src/main/kotlin/app/days/Day18.kt
+++ src/main/kotlin/app/days/Day18.kt
@@ -2,6 +2,18 @@ package app.days
 
 import app.factory.services.LumberManager
 import app.util.InputReader
+import com.varabyte.kotter.foundation.anim.Anim
+import com.varabyte.kotter.foundation.firstSuccess
+import com.varabyte.kotter.foundation.runUntilSignal
+import com.varabyte.kotter.foundation.session
+import com.varabyte.kotter.foundation.text.ColorLayer
+import com.varabyte.kotter.foundation.text.black
+import com.varabyte.kotter.foundation.text.text
+import com.varabyte.kotter.foundation.text.textLine
+import com.varabyte.kotter.foundation.timer.addTimer
+import com.varabyte.kotter.runtime.terminal.TerminalSize
+import com.varabyte.kotter.terminal.system.SystemTerminal
+import com.varabyte.kotter.terminal.virtual.VirtualTerminal
 import mu.KotlinLogging
 
 class Day18: IDay {
@@ -30,10 +42,92 @@ class Day18: IDay {
 
         val lumberManager2 = LumberManager(xSize, ySize, 3, input.map { it.toList() })
 
-        for(i in 1 .. 1000000000) {
-            lumberManager2.passMinute()
+        render(lumberManager2)
+
+        if(lumberManager2.cycle.first > 0) {
+            val cycle = lumberManager2.cycle
+            val cycleLength = lumberManager2.minute - cycle.second
+            val remainingMinutes = 1_000_000_000 - cycle.first
+            val remainingMinutesInCycle = remainingMinutes % cycleLength
+
+            for (i in 1 .. remainingMinutesInCycle) {
+                lumberManager2.passMinute()
+            }
         }
 
-        logger.info { lumberManager.getLumberyards() * lumberManager.getWoodAcres() }
+        logger.info { lumberManager2.getLumberyards() * lumberManager2.getWoodAcres() }
+    }
+
+    private fun render(lumberManager: LumberManager) = session(
+        terminal = listOf(
+            { SystemTerminal() },
+            { VirtualTerminal.create(terminalSize = TerminalSize(lumberManager.xSize, lumberManager.ySize + 15)) }
+        ).firstSuccess(),
+        clearTerminal = true) {
+
+        section {
+            textLine("Day 18 - Part 02")
+            textLine()
+        }.run()
+
+        val cycles = hashMapOf<Int, Pair<Int, Int>>()
+
+        section {
+
+            textLine("Minute: ${lumberManager.minute}")
+            black(ColorLayer.BG, isBright = true)
+            for(y in 0 until lumberManager.ySize) {
+                for(x in 0 until lumberManager.xSize) {
+                    val c = lumberManager.lumberCollectionArea.arrayMap[y, x]
+                    scopedState {
+                        text(c)
+                    }
+                }
+                textLine()
+            }
+
+        }.runUntilSignal {
+            addTimer(Anim.ONE_FRAME_60FPS, repeat = true) {
+                lumberManager.passMinute()
+                rerender()
+
+                if (lumberManager.lastRuns.size > 1) {
+                    for(i in lumberManager.lastRuns.size - 1 downTo 1) {
+                        if(lumberManager.lastRuns[i].array.contentDeepEquals(lumberManager.lastRuns[0].array)) {
+                            val cycle = i
+
+                            if(cycles[cycle] != null) {
+                                cycles[cycle] = cycles[cycle]!!.copy(second = cycles[cycle]!!.second + 1)
+                            } else {
+                                cycles[cycle] = Pair( lumberManager.minute, 1)
+                            }
+
+                            //logger.info { "Cycle detected at minute ${lumberManager.minute}, cycle length: $cycle" }
+                            break
+                        }
+                        rerender()
+                    }
+                }
+
+                if (lumberManager.minute % 100 == 0) {
+                    logger.info { cycles }
+                }
+
+                if(lumberManager.minute == 1000) {
+                    logger.info { cycles }
+
+                    if (!cycles.isEmpty()) {
+                        val maxCycle = cycles.maxBy { it.value.second }
+
+                        logger.info { "Max cycle: $maxCycle" }
+
+                        lumberManager.cycle = maxCycle.key to maxCycle.value.first
+                    }
+
+                    signal()
+                }
+            }
+        }
+
     }
 }
--- src/main/kotlin/app/factory/services/LumberManager.kt
+++ src/main/kotlin/app/factory/services/LumberManager.kt
@@ -4,9 +4,12 @@ import app.factory.model.LumberCollectionArea
 import app.model.Array2D
 import app.model.MatrixWindow
 
-class LumberManager(xSize: Int, ySize: Int, windowSize: Int) {
+class LumberManager(val xSize: Int, val ySize: Int, windowSize: Int) {
     val lumberCollectionArea = LumberCollectionArea(xSize, ySize)
     val window = MatrixWindow(windowSize)
+    var minute = 0
+    val lastRuns = ArrayDeque<Array2D<Char>>()
+    var cycle = Pair<Int, Int>(0, 0)
 
     constructor(xSize: Int, ySize: Int, windowSize: Int, mapInput: List<List<Char>>) : this(xSize, ySize, windowSize) {
         lumberCollectionArea.fillMap(mapInput)
@@ -43,6 +46,14 @@ class LumberManager(xSize: Int, ySize: Int, windowSize: Int) {
         lumberCollectionArea.arrayMap.forEachIndexed { y, x, _ ->
             lumberCollectionArea.arrayMap[y, x] = newMap[y, x]
         }
+
+        if(lastRuns.size == 100) {
+            lastRuns.removeLast()
+        }
+
+        lastRuns.addFirst(newMap)
+
+        minute += 1
     }
 
     fun getWoodAcres(): Int {
